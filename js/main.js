1016e8b46c540f0733e360742f9af3ce4cb3941b
somaia96 <88092847+somaia96@users.noreply.github.com>
2022-12-19 02:14:00 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+// fetch json file of words information
 fetch("js/words.json")
 .then(res=>res.json())
 .then((res)=>{
@@ -19,72 +20,93 @@ fetch("js/words.json")
     // the word div
     let wordDiv = document.querySelector(".word");
     for(let i =0;i<spansNum;i++){
+
         // create lines of word's letters number
         let span = document.createElement("span");
         wordDiv.appendChild(span);
     }
 
-    // write the word in the right place
 
-    // let spansLetters = document.querySelectorAll(".word span")
-    // let e =document.querySelector(".word span")
-    // let s = window.getComputedStyle(e,"before")
-    // let content =s["content"];/////////////////////////////////////////////////////////////////////////
-
-
-    for(let i=0;i< spansNum;i++){
-        // spansLetters[i].style.content=`${res[index].word[i]}`;
-        // console.log(spansNum);
-        // console.log(spansLetters);
-        // console.log(spansLetters[i]);
-        // console.log(res[index].word[i]);
-    }
-
+    let spansLetters = document.querySelectorAll(".word span");
     name.innerHTML=`Name: ${user}`;
-    let counter =0;
+
+    // the counter of false letter
+    let counterFalse =0;
     let hang= document.querySelector(".hang");
     window.addEventListener("click",function(e){
         let word = res[index].word.toUpperCase();
+
+        // click on letter
         if(e.target.className == "letter"){
+
+            // if the letter is true
             if(word.includes(e.target.innerHTML)){
                 e.target.style.backgroundColor="#fff133";
-                // console.log(res[index].word.indexOf(e.target.innerHTML.toLowerCase()));
-                
+
+                // counter of true letters
+                let classTrue =0;
+                for(let i=0;i< spansNum;i++){
+                    if(res[index].word[i] == e.target.innerHTML.toLowerCase()){
+                        spansLetters[i].setAttribute("data-before",res[index].word[i]);
+                        spansLetters[i].classList.add("true");
+                    }
+                    // add to the counter of true letters
+                    if(spansLetters[i].classList.contains("true")){
+                        classTrue++;
+
+                        // the win div
+                        if(classTrue == spansNum){
+                            document.querySelector(".end").innerHTML="You Won";
+                            document.querySelector(".end").style.cssText="background-image: linear-gradient(45deg , #000 20%, 40%,black 100%);color: #fff133;text-shadow: 0px 0px 20px #fff133;display:flex";                
+                            document.body.style.cssText="pointer-events: none";
+                        }
+                    }
+                }           
+            // if the letter is false
             }else{
-            e.target.style.filter="grayscale(1)";
+
+                // change background color of false letter
+                e.target.style.filter="grayscale(1)";
                 e.target.classList.add("done");
-                counter++;
-                console.log(counter);
-                if(counter < 7){
-                let part = document.createElement("div");
-                part.className="part";
-                hang.appendChild(part);
+                counterFalse++;
+                if(counterFalse < 7){
+
+                    // create the first five parts
+                    let part = document.createElement("div");
+                    part.className="part";
+                    hang.appendChild(part);
                 }
-                if(counter == 7){
-                let part = document.createElement("div");
-                part.className="handPart";
-                let body = document.querySelector(".part:nth-of-type(6)");
-                body.appendChild(part);
+                if(counterFalse == 7){
+
+                    // create hand part
+                    let part = document.createElement("div");
+                    part.className="handPart";
+                    let body = document.querySelector(".part:nth-of-type(6)");
+                    body.appendChild(part);
                 }
-                if(counter == 8){
-                let part = document.createElement("div");
-                part.className="lastPart";
-                let body = document.querySelector(".part:nth-of-type(6)");
-                body.appendChild(part);
-                document.querySelector(".end").style.display="flex";
-                document.body.style.cssText="pointer-events: none";
+                if(counterFalse == 8){
+
+                    // create feet part
+                    let part = document.createElement("div");
+                    part.className="lastPart";
+                    let body = document.querySelector(".part:nth-of-type(6)");
+                    body.appendChild(part);
+
+                    // show loss div
+                    document.querySelector(".end").style.display="flex";
+                    document.body.style.cssText="pointer-events: none";
                 }
-            // console.log(e.target.innerHTML);
             }
         }
+
     })
 
 })
 
+// choose the main color of page
 let colors = document.querySelectorAll(".color");
 colors.forEach((e)=>{
     e.onclick= function(){
-        // console.log(e.classList[1]);
         if(e.classList[1] == "green"){
             document.body.style.cssText="--hangColor:#3e894a;--personColor:#09721a;";
         }
